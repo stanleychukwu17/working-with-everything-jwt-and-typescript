64f8b180b9d60918a1b116394e25151a97ffc088
stanley chukwu <stanleychukwu17@yahoo.com>
2022-08-27 10:25:18 +0100

    opened the route for a user to login changed the lifetime of the cookies

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createSession, getUser, invalidateSession } from "../db";
+import { createSession, getUser, invalidateSession } from "../db/";
 import { signJWT, verifyJWT } from "../utils/jwt.utils";
 
 // login handler
@@ -8,23 +8,24 @@ export function createSessionHandler(req: Request, res: Response) {
     const user = getUser(email);
 
     if (!user || user.password !== password) {
-    return res.status(401).send("Invalid email or password");
+        return res.status(401).send("Invalid email or password");
     }
 
     const session = createSession(email, user.name);
+    const {sessionId} = session
 
     // create access token
     const accessToken = signJWT(
-        { email: user.email, name: user.name, sessionId: session.sessionId },
-        "5s"
+        {email: user.email, name: user.name, sessionId: sessionId},
+        "24h"
     );
 
-    const refreshToken = signJWT({ sessionId: session.sessionId }, "1y");
+    const refreshToken = signJWT({sessionId: sessionId}, "1y");
 
     // set access token in cookie
     res.cookie("accessToken", accessToken, {
-        maxAge: 300000, // 5 minutes
-        httpOnly: true,
+        maxAge: 3000000, // 50 minutes
+        httpOnly: true, // using httpOnly cookie is recommended instead of using local storage
     });
 
     res.cookie("refreshToken", refreshToken, {
